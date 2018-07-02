ea0989cf7eaeba2f3c99979f422f68d1986da6c1
imtianx <imtianx@gmail.com>
2018-07-02 18:01:50 +0800

    update deps;

--- src/main/java/Dependencies.kt
+++ src/main/java/Dependencies.kt
@@ -14,6 +14,10 @@ object Versions {
     const val android_sdk_target = 27
     const val android_sdk_support = "27.1.1"
 
+    const val junit = "4.12"
+    const val test_runner = "1.0.2"
+    const val espresso_core = "3.0.2"
+
     const val multidex = "1.0.3"
 
     const val kotlin = "1.2.41"
@@ -21,6 +25,20 @@ object Versions {
 
     const val constraintlayout = "1.1.2"
 
+    const val retrotit2 = "2.4.0"
+    const val gson = "2.8.5"
+    const val rxjava2 = "2.1.16"
+    const val rxandroid = "2.0.2"
+
+    const val glid = "4,7.1"
+    const val eventbus = "3.1.1"
+
+    const val arouter_api = "1.3.1"
+    const val arouter_compile = "1.1.4"
+    const val arouter_annotation = "1.0.4"
+    const val arouter_register = "1.0.2"
+
+    const val fragmentation = "1.3.5"
 
 }
 
@@ -38,6 +56,11 @@ object Deps {
 
     const val constraintlayout = "com.android.support:multidex:${Versions.constraintlayout}"
 
+    // test
+    const val junit = "junit:junit:${Versions.junit}"
+    const val test_runner = "com.android.support.test:runner:${Versions.test_runner}"
+    const val espresso_core = "com.android.support.test.espresso:espresso-core:${Versions.espresso_core}"
+
     // kotlin plugin,jre7,noarg,allopen
     const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlin_stdlib_jre7 = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
@@ -51,6 +74,32 @@ object Deps {
 
     // databinding compile ,the version same as android_plugin
     const val databinding_compile = "com.android.databinding:compiler:${Versions.android_plugin}"
+
+    // retrofit
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrotit2}"
+    const val retrofit2_convert_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrotit2}"
+    const val retrofit2_adapter_rxjava2 = "com.squareup.retrofit2:adapter-rxjava:${Versions.retrotit2}}"
+
+    // rxandroid,rxjava2
+    const val rxjava2 = "io.reactivex:rxjava:${Versions.rxjava2}"
+    const val rxandroid = "io.reactivex:rxandroid:${Versions.rxandroid}"
+
+    // gson
+    const val gson = "com.google.code.gson:gson:${Versions.gson}"
+
+    // glide
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glid}"
+
+    // eventbus
+    const val eventbus = "org.greenrobot:eventbus:${Versions.eventbus}"
+
+    // fragmentation
+    const val fragmentation = "me.yokeyword:fragmentation:${Versions.fragmentation}"
+
+
+    // arouter
+    const val arouter_api = "'com.alibaba:arouter-api:${Versions.arouter_api}"
+    const val arouter_compile = "'com.alibaba:arouter-compile:${Versions.arouter_compile}"
 
 
 }
